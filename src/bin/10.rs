ce9101a9eb22547c6601d0cd7d59570c1aed21cd
Andrew Hirschi <31867489+ashirsc@users.noreply.github.com>
2023-04-13 02:57:17 +0000

    finished 10 part two

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -9,47 +9,69 @@ fn main() {
     file.read_to_string(&mut contents)
         .expect("Failed to read file");
 
-    let mut register = 1;
-    let mut signal_strengths:Vec<i32> = Vec::new();
-    let mut cycle_count = 1;
+    let mut register:i32 = 1;
+    let mut signal_strengths: Vec<i32> = Vec::new();
     let mut processing = false;
     let mut inc = 0;
+    let mut row = String::new();
+    let mut output:Vec<String> = vec![];
 
     let mut lines = contents.lines();
+    print_sprite(&register);
 
-    loop {
-        if ((cycle_count - 20) % 40) == 0 {
-            println!("cycle: {}, strength: {}",cycle_count, register);
-            signal_strengths.push(cycle_count * register);
+
+    for cycle in 0..240 {
+        if cycle % 40 == 0 {
+            output.push(row);
+            row = String::new();
         }
 
+        //draw pixel
+        let mut pixel = ' ';
+        if row.len() as i32 >= register - 1 && row.len() as i32 <= register  + 1 {
+            pixel = '#'
+        }
+
+        row.push(pixel);
+        
         if processing {
             register += inc;
-            cycle_count+=1;
             processing = false;
-            continue;
+            print_sprite(&register);
+        } else {
+            let mut cmd = match lines.next() {
+                Some(l) => l.split_whitespace(),
+                None => break,
+            };
+
+            match cmd.next() {
+                Some("addx") => {
+                    inc = cmd.next().unwrap().parse().unwrap();
+                    processing = true;
+                }
+                Some("noop") => (),
+                _ => panic!("Unknown instruction"),
+            }
+
         }
-
-        let mut cmd = match lines.next() {
-            Some(l) => l.split_whitespace(),
-            None => break
-        };
-
-        match cmd.next() {
-            Some("addx") => {
-                inc = cmd.next().unwrap().parse().unwrap();
-                processing = true;
-            },
-            Some("noop") => (),
-            _ => panic!("Unknown instruction")
-        }
-
-
-        cycle_count += 1;
     }
+    output.push(row);
 
-    
-    println!("Sum signal strength: {} ", signal_strengths.into_iter().sum::<i32>())
+    println!("final output");
+   for l in output {
+    println!("{}", l);
+   }
 }
 
+fn print_sprite(x: &i32) {
+    let mut row = String::new();
+    for i in 0..40 {
+        if i >= x - 1 && i <= x + 1 {
+            row.push('#')
+        } else {
+            row.push('.')
+        }
+    }
 
+    println!("{}", row);
+}
